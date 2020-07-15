5072d05e31067593eedca09f0e745e2c8bccbf14
MichaelS <michael.sandovalm20gmail.com>
2020-07-15 17:14:22 -0400

    Se añadieron los archivos de priettier y eslint. Falta configuracion

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require ('express');
 const app = express();
+const morgan = require ('morgan');
+const cors = require ('cors');
+
+app.use(cors());
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
