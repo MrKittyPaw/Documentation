b5c37be7b5dad72873e2c4466f7dff98120aa4f6
MichaelS <michael.sandovalm20gmail.com>
2020-07-21 20:49:01 -0400

    Se agregaron las funciones del json web token

--- index.js
+++ index.js
@@ -3,9 +3,36 @@ const jwt = require ('jsonwebtoken');
 const morgan = require ('morgan');
 require ('dotenv').config();
 const cors = require ('cors');
+const { json } = require('express');
 const app = express();
 
-console.log(process.env.PORT);
+const PORT = process.env.PORT;
+
+app.post('/login', (req, res) => {
+    const u = {id: 1};
+    const token = jwt.sign({u}, 'my_secret_token');
+    res.json({
+        token
+    });
+});
+
+function comToken(req, res, next){
+    const header = req.headers['authorization'];
+    if(typeof header !== 'undefined'){
+        const head = header.split(" ");
+        const headToken = head[1];
+        req.token = headToken;
+        next();
+    }else {
+        res.sendStatus(403);
+    }
+}
+
+app.get('/private', comToken, (req, res) => {
+    res.json({
+        text: 'Hello world private'
+    });
+});
 
 app.use(cors());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
